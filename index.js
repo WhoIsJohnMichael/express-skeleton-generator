89e3a031e9436696d9adfcb3a857f65cd7c93d54
John Michael Santos <santos.johnmichael.t.3489@gmail.com>
2024-04-21 21:17:40 +0800

    Fixed indentation error

--- index.js
+++ index.js
@@ -11,61 +11,60 @@ async function generateProjectSkeleton(projectName) {
 
     const appFilePath = path.join(projectPath, "app.js");
     const appContent = `
-    import express from "express";
-    import pug from "pug";
-    import morgan from "morgan";
-    import path, { dirname } from "path";
-    import { fileURLToPath } from "url";
-    import createHttpError from "http-errors";
-    import cookieParser from "cookie-parser";
-    import helmet from "helmet";
-    import { rateLimit } from "express-rate-limit"
+import express from "express";
+import pug from "pug";
+import morgan from "morgan";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import createHttpError from "http-errors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit"
     
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    const app = express();
-    const port = process.env.PORT || 3001;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = process.env.PORT || 3001;
     
-    import indexRouter from "./routes/index.js";
-    
-    //template-engine: pug see docs at https://pugjs.org
-    app.set("view engine", pug);
-    
-    //middleware setup
-    app.use(morgan("dev"));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.static(path.join(__dirname, "public")));
-    app.use(cookieParser());
-    app.use(helmet());
-    
-    // security limiting request just comment it if youre still in development
-    const limiter = rateLimit({
-      windowMs: 1 * 60 * 1000,
-      max: 20
-    });
-    app.use(limiter)
-    
-    // Routes setup
-    app.use("/", indexRouter);
-    
-    // Custom middleware for handling 404 errors
-    app.use((req, res, next) => {
-      next(createHttpError(404, "Not Found"));
-    });
-    
-    // Error handling middleware
-    app.use((err, req, res, next) => {
-      const status = err.status || 500;
-      const message = err.message || "Internal Server Error";
-      const err_msg = req.app.get("env")  === 'development' ? err : '';
-      res.render("error.pug", { status: status, message: message, error: err_msg });
-    });
-    
-    app.listen(port, () => {
-      console.log(\`Application is running at port : \${port}\`);
-    });
-    `;
+import indexRouter from "./routes/index.js";
 
+//template-engine: pug see docs at https://pugjs.org
+app.set("view engine", pug);
+
+//middleware setup
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(helmet());
+
+// security limiting request just comment it if youre still in development
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20
+});
+app.use(limiter)
+
+// Routes setup
+app.use("/", indexRouter);
+
+// Custom middleware for handling 404 errors
+app.use((req, res, next) => {
+  next(createHttpError(404, "Not Found"));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  const err_msg = req.app.get("env")  === 'development' ? err : '';
+  res.render("error.pug", { status: status, message: message, error: err_msg });
+});
+
+app.listen(port, () => {
+  console.log(\`Application is running at port : \${port}\`);
+});
+`;
     await writeFile(appFilePath, appContent);
 
     const packageJson = {
@@ -106,41 +105,41 @@ async function generateProjectSkeleton(projectName) {
 
     const routesPath = path.join(projectPath, "routes", "index.js");
     const routesContent = `
-      import express from 'express'
-      const router = express.Router()
+import express from 'express'
+const router = express.Router()
 
-      /* GET home page. */
-      router.get('/', (req, res) => {
-      res.render('index.pug', { title: 'Hey', message: 'Hello there!' })
-      });
+/* GET home page. */
+router.get('/', (req, res) => {
+res.render('index.pug', { title: 'Hey', message: 'Hello there!' })
+});
 
-      export default router;
-    `;
+export default router;
+`;
 
     await writeFile(routesPath, routesContent);
 
     const viewIndexPath = path.join(projectPath, "views", "index.pug");
     const viewIndexContent = `
-        doctype html
-        html(lang="en")
-        head
-            meta(charset="UTF-8")
-            meta(name="viewport", content="width=device-width, initial-scale=1.0")
-            title= title
-        body 
-        h1= message
-    `;
+doctype html
+html(lang="en")
+  head
+    meta(charset="UTF-8")
+    meta(name="viewport", content="width=device-width, initial-scale=1.0")
+    title= title
+  body 
+    h1= message
+`;
 
     await writeFile(viewIndexPath, viewIndexContent);
 
     const viewErrorPath = path.join(projectPath, "views", "error.pug");
     const viewErrorContent = `
-        h1= status 
-        p= message 
-        hr
-        if error
-            p= error
-    `;
+h1= status 
+p= message 
+hr
+if error
+  p= error
+`;
 
     await writeFile(viewErrorPath, viewErrorContent);
 
